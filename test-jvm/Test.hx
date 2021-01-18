22c3d97d9d649127bda29c3c536fef05dbc2d4ed
zhengxiaoyao0716 <1499383852@qq.com>
2021-01-19 01:11:48 +0800

    wtf

--- test-jvm/Test.hx
+++ test-jvm/Test.hx
@@ -1,13 +1,24 @@
+import haxe.iterators.ArrayIterator;
+
 typedef Typedef1<V> = {function next():V;};
 typedef Typedef2<V> = {function next():V;};
+typedef TestHasNext<V> = {function hasNext():Bool;};
+typedef TestHasNextNoGeneric = {function hasNext():Bool;};
+typedef TestIterator<V> = {function iterator():V;};
+typedef TestIteratorNoGeneric = {function iterator():Int;}; // no `$Interface`
+typedef TestOtherFunc<V> = {function func():V;}; // no `$Interface`
+typedef MyIterable<V> = {iterator:() -> ArrayIterator<V>};
+typedef MyIterableInherit<V> = {> Iterable<V>, arrayIterator:() -> ArrayIterator<V>};
 
 class Test {
 	public static function main() {
 		var obj:Typedef2<String> = {next: () -> "test typedef with generic and `next` function"};
-		test(cast(obj:Typedef1<String>)); // `jvm` target not works?
-	}
+		trace(cast(obj : Typedef1<String>)); // `jvm` target not works?
 
-	static function test<T>(obj:Typedef1<T>) {
-		trace("[SUCCEED] " + obj.next());
+		var iter:MyIterable<Int> = {iterator: () -> [].iterator()};
+		trace(cast(iter : Iterable<Int>)); // not inherit `Iterable$Interface`?
+
+		var iter:MyIterableInherit<Int> = {iterator: () -> [].iterator(), arrayIterator: () -> [].iterator()};
+		trace(cast(iter : Iterable<Int>)); // WTF?
 	}
 }
